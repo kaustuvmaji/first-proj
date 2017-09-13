d76935689bc003f816a540e8dc2f0edf637a41ac
kaustuv Maji <kaustuv.maji@gmail.com>
2017-09-13 16:37:19 +0530

    Authentication

--- src/main/java/com/example/demo/EmployeeRestController.java
+++ src/main/java/com/example/demo/EmployeeRestController.java
@@ -8,6 +8,7 @@ import java.util.Collection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.Authorization;
 
 /**
  * Employee management services
@@ -24,17 +26,19 @@ import io.swagger.annotations.ApiOperation;
  *
  */
 @RequestMapping("/employee/services")
-@Api(value = "/employee/services", description = "Employee management services",produces = MediaType.APPLICATION_JSON_VALUE)
+@Api(value = "/employee/services", description = "Employee management services", produces = MediaType.APPLICATION_JSON_VALUE)
 @RestController()
 public class EmployeeRestController {
 
 	@Autowired
 	EmployeeService employeeService;
 
-	@RequestMapping(value = "/listOfEmployee", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE})
+	@RequestMapping(value = "/listOfEmployee", method = RequestMethod.GET, produces = {
+			MediaType.APPLICATION_JSON_VALUE })
 	@ResponseBody
 	@Secured("ROLE_USER")
-	@ApiOperation(value = "All employee details", notes = "Avialable employees", response = Employee.class, responseContainer = "List", produces = MediaType.APPLICATION_JSON_VALUE)
+	@ApiOperation(value = "All employee details", notes = "Avialable employees", response = Employee.class, responseContainer = "List", produces = MediaType.APPLICATION_JSON_VALUE, authorizations = {
+			@Authorization(value = "basic"/*"security scope bounded to 'ROLE_USER' users "*/) })
 	Collection<Employee> getEmployees() {
 		return employeeService.getEmployees();
 	}
@@ -42,7 +46,8 @@ public class EmployeeRestController {
 	@RequestMapping(value = "/employeeDetail", method = { org.springframework.web.bind.annotation.RequestMethod.GET })
 	@ResponseBody
 	@Secured("ROLE_ADMIN")
-	@ApiOperation(value = "Employee detail by id", notes = "Employee detail", response = Employee.class)
+	@ApiOperation(value = "Employee detail by id", notes = "Employee detail", response = Employee.class, authorizations = {
+			@Authorization(value = "security scope bounded to 'ROLE_ADMIN' users ") })
 	Employee getEmployee(@RequestParam("id") Integer id) {
 		return employeeService.getEmployee(id);
 	}
@@ -50,7 +55,8 @@ public class EmployeeRestController {
 	@RequestMapping(value = "/addEmployee", method = {
 			org.springframework.web.bind.annotation.RequestMethod.POST }, produces = MediaType.APPLICATION_JSON_VALUE)
 	@ResponseBody
-	@ApiOperation(value = "Add Employee", notes = "Employee add", response = Employee.class, produces = MediaType.APPLICATION_JSON_VALUE)
+	@ApiOperation(value = "Add Employee", notes = "Employee add", response = Employee.class, produces = MediaType.APPLICATION_JSON_VALUE, authorizations = {
+			@Authorization(value = "security scope bounded to 'ROLE_ADMIN' users ") })
 	@Secured("ROLE_ADMIN")
 	Employee addEmployees(@RequestParam("id") Integer id, @RequestParam("name") String name,
 			@RequestParam(value = "department", required = false, defaultValue = "dev") String department) {
@@ -61,16 +67,18 @@ public class EmployeeRestController {
 			org.springframework.web.bind.annotation.RequestMethod.DELETE })
 	@ResponseBody
 	@Secured("ROLE_ADMIN")
-	@ApiOperation(value = "Employee deletion", notes = "Employee delete")
+	@ApiOperation(value = "Employee deletion", notes = "Employee delete", authorizations = {
+			@Authorization(value = "security scope bounded to 'ROLE_ADMIN' users ") })
 	void deleteEmployee(@RequestParam("id") Integer id) {
 		/* return */ employeeService.deleteEmployee(id);
 	}
 
 	@RequestMapping(value = "/updateEmployee", method = {
-			org.springframework.web.bind.annotation.RequestMethod.PUT },produces = MediaType.APPLICATION_JSON_VALUE)
+			org.springframework.web.bind.annotation.RequestMethod.PUT }, produces = MediaType.APPLICATION_JSON_VALUE)
 	@ResponseBody
-	@Secured("ROLE_ADMIN")
-	@ApiOperation(value = "Employee details update", notes = "present scope: Only department details updation", response = Employee.class, produces = MediaType.APPLICATION_JSON_VALUE)
+	@PreAuthorize("hasAuthority('ROLE_ADMIN')")
+	@ApiOperation(value = "Employee details update", notes = "present scope: Only department details updation", response = Employee.class, produces = MediaType.APPLICATION_JSON_VALUE, authorizations = {
+			@Authorization(value = "security scope bounded to 'ROLE_ADMIN' users ") })
 	Employee updateEmployee(@RequestParam("id") Integer id, @RequestParam(value = "name", required = false) String name,
 			@RequestParam(value = "department", required = true) String department) {
 		return employeeService.updateEmployee(id, name, department);
--- src/main/java/com/example/demo/RestAppSecurity.java
+++ src/main/java/com/example/demo/RestAppSecurity.java
@@ -3,7 +3,6 @@ package com.example.demo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,40 +10,52 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+// @Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(securedEnabled = true)
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true, jsr250Enabled = true, order = SecurityProperties.ACCESS_OVERRIDE_ORDER)
 public class RestAppSecurity extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	RestAppAuthenticationProvider restAppAuthenticationProvider;
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.springframework.security.config.annotation.web.configuration.
+	 * WebSecurityConfigurerAdapter#configure(org.springframework.security.config.
+	 * annotation.authentication.builders.AuthenticationManagerBuilder)
+	 */
+	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
 		/*
 		 * Step 1. Pure example of inMemoryAuthentication
 		 */
+		 //	auth.inMemoryAuthentication().withUser("admin").password("admin@123").roles("ADMIN"). // Admin user
+		 //			and().withUser("user1").password("user@123").roles("USER"). // local user
+		 //			and().withUser("kaustuv").password("pass@123").roles("USER", "ADMIN"); // Myself having both
 
-		/*
-		 * auth.inMemoryAuthentication().withUser("admin").password("admin@123").roles(
-		 * "ADMIN"). // Admin user
-		 * and().withUser("user1").password("user@123").roles("USER"). // local user
-		 * and().withUser("kaustuv").password("pass@123").roles("USER", "ADMIN"); //
-		 * Myself having both
-		 */
 		/*
 		 * Step 2. Example of AuthenticationProvider
 		 */
 		auth.authenticationProvider(restAppAuthenticationProvider);
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.springframework.security.config.annotation.web.configuration.
+	 * WebSecurityConfigurerAdapter#configure(org.springframework.security.config.
+	 * annotation.web.builders.HttpSecurity)
+	 */
+	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		// Roles based authentication using url
 		/*
-		 * http.httpBasic().and().authorizeRequests().antMatchers("/employee/**").
-		 * hasRole("ADMIN").antMatchers("/**")
+		 *  http.httpBasic().and().authorizeRequests().antMatchers("/employee/**").
+		 *  hasRole("ADMIN").antMatchers("/**")
 		 * .hasRole("USER").and().csrf().disable().headers().frameOptions().disable();
 		 */
-
 		http.httpBasic().and().authorizeRequests().anyRequest().authenticated().and().csrf().disable().headers()
 				.frameOptions().disable();
 	}
