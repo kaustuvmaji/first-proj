afcc623eb7272ecf59de2ddf95310f86e8a64173
kaustuv Maji <kaustuv.maji@gmail.com>
2017-09-22 15:16:11 +0530

    pass

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 // things in xml.
 @ComponentScan(basePackages = { "com.example.demo", "com.example.demo.app.configuration",
 		"com.example.demo.application", "com.example.demo.application.aop", "com.example.demo.restinterface",
-		"com.example.demo.scheduler", "com.example.demo.security"/*, "com.example.demo.app.management"*/ })
+		"com.example.demo.scheduler", "com.example.demo.security", "com.example.demo.app.management" })
 @EnableAutoConfiguration
 public class DemoApplication {
 
