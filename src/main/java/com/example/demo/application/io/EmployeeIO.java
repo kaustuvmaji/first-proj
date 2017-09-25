a71c7c1d1b388eecc362ff6accf53596bf1f3719
kaustuv Maji <kaustuv.maji@gmail.com>
2017-09-25 11:48:42 +0530

    Removed Employee Object Mocking.

--- src/main/java/com/example/demo/application/io/EmployeeIO.java
+++ src/main/java/com/example/demo/application/io/EmployeeIO.java
@@ -3,26 +3,86 @@
  */
 package com.example.demo.application.io;
 
-import com.example.demo.domain.Employee;
-
 /**
  * @author kaust
  *
  */
-public class EmployeeIO extends Employee {
+public class EmployeeIO {
 
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDepartment() {
+		return department;
+	}
+	public void setDepartment(String department) {
+		this.department = department;
+	}
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1602661192445633249L;
-
-	/**
-	 * @param id
-	 * @param name
-	 * @param department
-	 */
+	
+	private Integer id;
+	private String name;
+	private String department;
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((department == null) ? 0 : department.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		EmployeeIO other = (EmployeeIO) obj;
+		if (department == null) {
+			if (other.department != null)
+				return false;
+		} else if (!department.equals(other.department))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "EmployeeIO [id=" + id + ", name=" + name + ", department=" + department + "]";
+	}
 	public EmployeeIO(Integer id, String name, String department) {
-		super(id, name, department);
+		super();
+		this.id = id;
+		this.name = name;
+		this.department = department;
 	}
 
+	
+
+
+	
 }
--- src/test/java/com/example/demo/EmployeeServiceMockitoTests.java
+++ src/test/java/com/example/demo/EmployeeServiceMockitoTests.java
@@ -1,8 +1,8 @@
 package com.example.demo;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
@@ -11,13 +11,12 @@ import org.springframework.test.context.junit4.SpringRunner;
 import com.example.demo.domain.Employee;
 import com.example.demo.domain.EmployeeService;
 
-import org.junit.Assert;
-
 @ActiveProfiles("test")
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class EmployeeServiceMockitoTests {
 
+	private final Employee rev = new Employee(2, "Rev", "qa");
 	@Autowired
 	EmployeeService employeeService;
 
@@ -26,29 +25,20 @@ public class EmployeeServiceMockitoTests {
 
 	@Test
 	public void testGetEmployee() {
-		Mockito.when(employeeService.getEmployee(1)).thenReturn(new Employee(1, "Josh", "dev"));
-		Mockito.when(employeeService.getEmployee(2)).thenReturn(new Employee(2, "Rev", "qa"));
 		Assert.assertEquals("Josh", employeeService.getEmployee(1).getName());
-		// Mockito.verify(employeeService,
-		// Mockito.times(1)).count(Mockito.any(CriteriaDto.class));
 	}
 
 	@Test
 	public void testAddEmployee() {
-		Employee newlyAdded = new Employee(2, "Rev", "qa");
-		Mockito.when(employeeService.addEmployee(2, "Rev", "qa")).thenReturn(newlyAdded);
-		Employee addEmployees = employeeService.addEmployee(2, "Rev", "qa");
-		Assert.assertEquals("Rev", addEmployees.getName());
-		Assert.assertEquals("qa", addEmployees.getDepartment());
+		Employee addEmployees = employeeService.addEmployee(rev.getId(), rev.getName(), rev.getDepartment());
+		Assert.assertEquals(rev.getName(), addEmployees.getName());
+		Assert.assertEquals(rev.getDepartment(), addEmployees.getDepartment());
 	}
 
 	@Test
 	public void testUpdateEmployee() {
-		Employee newlyAdded = new Employee(2, "Rev", "dev");
-		Mockito.when(employeeService.updateEmployee(2, "rev", "dev")).thenReturn(newlyAdded);
-		Employee updateEmployees = employeeService.updateEmployee(2, "rev", "dev");
-		Assert.assertEquals("Rev", updateEmployees.getName());
-		Assert.assertNotEquals("qa", updateEmployees.getName());
+		Employee updateEmployees = employeeService.updateEmployee(rev.getId(), rev.getName(), "dev");
+		Assert.assertNotEquals(rev.getDepartment(), updateEmployees.getName());
 		Assert.assertEquals("dev", updateEmployees.getDepartment());
 	}
 }
