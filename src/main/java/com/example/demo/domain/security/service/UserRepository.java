6967caeacd1ea27656bee4353617605bf69fe1bc
kaustuv Maji <kaustuv.maji@gmail.com>
2017-11-09 13:36:52 +0530

    cache

--- src/main/java/com/example/demo/domain/security/service/UserRepository.java
+++ src/main/java/com/example/demo/domain/security/service/UserRepository.java
@@ -1,5 +1,6 @@
 package com.example.demo.domain.security.service;
 
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,6 +9,7 @@ import com.example.demo.domain.security.AppUser;
 @Repository
 public interface UserRepository extends MongoRepository<AppUser, String> {
 
+	@Cacheable(value = "userCache", key = "userName", sync = true)
 	AppUser findByUserName(String userName);
-	
+
 }
