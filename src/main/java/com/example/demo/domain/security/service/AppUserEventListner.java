b48aba8b203c7c4801bdee1886b90148968086f7
kaustuv Maji <kaustuv.maji@gmail.com>
2017-11-09 13:13:30 +0530

    add to context

--- src/main/java/com/example/demo/domain/security/service/AppUserEventListner.java
+++ src/main/java/com/example/demo/domain/security/service/AppUserEventListner.java
@@ -1,19 +1,25 @@
 package com.example.demo.domain.security.service;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 
 import com.example.demo.domain.event.AppUserEvent;
+import com.example.demo.domain.security.AppUser;
 
 @Component
 public class AppUserEventListner implements ApplicationListener<AppUserEvent> {
+
+	Logger logger = Logger.getLogger(getClass());
 
 	@Autowired
 	UserRepository userRepository;
 
 	@Override
 	public void onApplicationEvent(AppUserEvent event) {
-		userRepository.save(event.getAppUser());
+		logger.info("Register users");
+		AppUser user = userRepository.save(event.getAppUser());
+		logger.info("new user " + user + "registered successfully");
 	}
 }
